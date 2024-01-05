405c8f875b959f8afacaf33674ba40d8720b2bd1
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-01-05 23:42:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -372,10 +372,12 @@ var renderMessage = ({ type, data, user, date, id }, i) => {
     elem.appendChild(footerElem)
 
     //render menu
-    var deleteMsgButton = document.createElement("div")
-    deleteMsgButton.classList.add("menu")
-    deleteMsgButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`
-    elem.appendChild(deleteMsgButton)
+    if (user == _user) {
+        var deleteMsgButton = document.createElement("div")
+        deleteMsgButton.classList.add("menu")
+        deleteMsgButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`
+        elem.appendChild(deleteMsgButton)
+    }
 
     if (type == "info" || type == "active user")
         elem.removeChild(deleteMsgButton)
