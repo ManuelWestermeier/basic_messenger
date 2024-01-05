ce11aaa370364e4da7f78c6112f44e633e45a472
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-01-05 23:46:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -371,12 +371,14 @@ var renderMessage = ({ type, data, user, date, id }, i) => {
     elem.appendChild(dataElem)
     elem.appendChild(footerElem)
 
-    //render menu
     if (user == _user) {
+        //render menu
         var deleteMsgButton = document.createElement("div")
         deleteMsgButton.classList.add("menu")
         deleteMsgButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`
         elem.appendChild(deleteMsgButton)
+        //chage color of my message
+        elem.classList.add("mymsg")
     }
 
     if (type == "info" || type == "active user")
@@ -387,9 +389,6 @@ var renderMessage = ({ type, data, user, date, id }, i) => {
 
     footerElem.title = "click to change name"
     footerElem.classList.add("msg-footer")
-
-    if (user == _user)
-        elem.classList.add("mymsg")
 
     footerElem.innerText =
         `${contacts?.[user] ?? user} ${date}`
@@ -422,8 +421,7 @@ var renderMessage = ({ type, data, user, date, id }, i) => {
             var data = data
                 .split("<script").join("< script")
                 .split("</script").join("< /script")
-            var data =
-                `<style>*{
+            var data = `<style>*{
             background-color: #e9f3ea;
             color: black;
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
